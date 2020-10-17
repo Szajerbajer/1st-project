e34ba40c18c23c1fea0ae69d6bc8e5bcb4ebcbe1
Marta Szajer <szajer.bajer@gmail.com>
2020-10-17 15:28:36 +0200

    add code to main js

---
+++ main.js
@@ -0,0 +1,15 @@
+const header = document.querySelector("header");
+const h1 = document.querySelector("h1");
+let image = true;
+let title = true;
+
+const changeImage = () => {
+  image = !image;
+  title = !title;
+  header.style.bacgroundImage = image
+    ? "url(siedzimy.jpg)"
+    : "url(czarno_biale.jpg)";
+  h1.textContent = title ? "Siatkarki plażowe" : "Siatkarki w akcji";
+};
+
+header.addEventListener("click", changeImage);
